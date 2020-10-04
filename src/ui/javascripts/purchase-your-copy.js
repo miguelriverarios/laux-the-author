68d39e08d8724a76659066cf5b71fad866eb9070
Miguel Rivera Rios <miguel.e.rivera.rios@gmail.com>
2020-10-04 15:39:20 -0400

    Debug

--- src/ui/javascripts/purchase-your-copy.js
+++ src/ui/javascripts/purchase-your-copy.js
@@ -3,7 +3,7 @@ const snackbarPurchase = $('.mdc-snackbar--purchase-success');
 if (snackbarPurchase.length) {
 const params = new URLSearchParams(window.location.search);
 const status = params.get('status');
-console.log(status);
+// console.log(status);
 if (status) {
     const mdcSnackbarPurchase = snackbarPurchase[0].MDCSnackbar;
     mdcSnackbarPurchase.open();
